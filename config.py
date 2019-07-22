dffe9214b236b6eaa0ec83f694651e8f893de684
Evan Carter <escarter@wesleyan.edu>
2019-07-22 17:51:06 -0500

    Update to auth_keys file input format
    updated config file to be cleaner while inputting credentials

--- config.py
+++ config.py
@@ -5,33 +5,43 @@ import tweepy
 logger = logging.getLogger()
 
 #set file path to upload OAUTH keys
-os_path   = "C:/Users/ecarter/projects/bots/StrongBot"
+os_path   = "D:/evnca/projects/StrongBot/"
 os_root   = "authkeys.txt"
 file_path = os.path.join(os_path+os_root)
 
 def create_api():
+
     #read in auth file contents
-    auth_file = open(file_path, 'r')
-    api_key       = auth_file.readline().rstrip('\n')
-    api_token    = auth_file.readline().rstrip('\n')
-    access_key    = auth_file.readline().rstrip('\n')
-    access_token = auth_file.readline().rstrip('\n')
+    auth_table = {}
+    auth_file  = open(file_path, 'r')
+    for line in auth_file:
+            
+        key, *values = line.rstrip('\n').split(':')
+        auth_table[key] = [value for value in values]
+    
     auth_file.close()
 
+    consumer_key        = str(auth_table['CONSUMER_KEY']).strip("'[]'")
+    consumer_secret     = str(auth_table['CONSUMER_SECRET']).strip("'[]'")
+    access_token        = str(auth_table['ACCESS_TOKEN']).strip("'[]'")
+    access_token_secret = str(auth_table['ACCESS_TOKEN_SECRET']).strip("'[]'")
+
     #attempt to authenticate credentials   
-    auth = tweepy.OAuthHandler(api_key, api_token)
-    auth.set_access_token(access_key, access_token)
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
 
     api = tweepy.API(auth, wait_on_rate_limit = True,
                      wait_on_rate_limit_notify = True)
 
+    logger.info("Authenticating...")
     try:
         api.verify_credentials()
-        print("Authentication is OK")
+        print("Authentication successful")
 
     except Exception as e:
         
         logger.error("Error creating API: Check credentials.", exc_info=True)
         raise e
+
     logger.info("API creation successful.")
     return api
--- keanu_test.py
+++ keanu_test.py
@@ -3,6 +3,9 @@ import logging
 import time
 import tweepy
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
 def check_mentions(api, keywords, since_id):
     
     logger.info("Retrieving mentions")
@@ -11,11 +14,13 @@ def check_mentions(api, keywords, since_id):
                                since_id=since_id).items():
         
         new_since_id = max(tweet.id, new_since_id)
-        if tweet.in_reply_to_status_id is not None:
-            if tweet.text.lower() == "you're breathtaking!":
+        if tweet.text is not None:
+            if all(keyword in tweet.text.lower() for keyword in keywords):
                 
+                #follow and favorite tweet if not already
                 if not tweet.user.following:
                     tweet.user.follow()
+                    tweet.favorite()
                     
                 api.update_status(status="YOU'RE breathtaking!",
                                   in_reply_to_status_id=tweet.id)
@@ -27,7 +32,7 @@ def main():
     api = create_api()
     since_id = 1
     print(since_id)
-    since_id = check_mentions(api, ["help", "support"], since_id)
+    since_id = check_mentions(api, ["you're", "breathtaking"], since_id)
 
-if __name__ == "MAIN":
+if __name__ == "__main__":
     main()
